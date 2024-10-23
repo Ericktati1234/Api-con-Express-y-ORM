85fe52f11b678a2ba0308ba8a013e7c1c17493ad
Ericktati1234 <91512625+Ericktati1234@users.noreply.github.com>
2024-10-22 23:47:24 -0600

    Update movies.js
    se eliminaron comentarios innecesarios

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const {Movie,Actor} = require('../db'); //con esas llaves en Genre, solo extraemos aquel que se llama igual
-//Ese Genre ya esta inicializado con el sequelize en el db
-
-//Ahora si vamos a darle vida a nuestra aplicacion
+const {Movie,Actor} = require('../db'); 
 
 function create(req, res, next) {
     const title = req.body.title;
@@ -39,12 +36,10 @@ function index(req, res, next) {
 }
 
 function replace(req, res, next) {
-    //EN este se definen parametros por el header y por el body, en el header quien, en el body con que vas a reemplazar
     const id = req.params.id;
     Genre.findByPk(id)
             .then(object => {
-                const description = req.body.description ? req.body.description : ""; //Aqui estamos diciendo si es que el nombre esta en el body
-                                                                    //la variable name es dicho valor, sino es nulo o ""
+                const description = req.body.description ? req.body.description : ""; 
                 const status = req.body.status ? req.body.status : null; 
                 object.update({
                     description: description,
@@ -58,8 +53,7 @@ function update(req, res, next) {
     const id = req.params.id;
     Genre.findByPk(id)
             .then(object => {
-                const description = req.body.description ? req.body.description : object.description; //Aqui estamos diciendo si es que el nombre esta en el body
-                //la variable name es dicho valor, sino es nulo o ""
+                const description = req.body.description ? req.body.description : object.description; 
                 const status = req.body.status ? req.body.status : object.status;
                 object.update({
                     description: description,
@@ -71,14 +65,11 @@ function update(req, res, next) {
 
 function destroy(req, res, next) {
     const id = req.params.id;
-    //EN esta funcion es necesario definir ese where ya que solamente buscamos borrar en donde el id sea igual a dicho id
-    //      segun la logica del negocio definiremos el parametro
+    
     Genre.destroy({ where: { id : id} }).then(object => res.json(object)).catch(ex => res.send(ex));
 }
 
-//Para cada uno de los metodos del protocolo http creamos una funcion en funcion del modelo restful
-//  dicha funcion, tiene dentro un send, que envia el metodo del protocolo, con la ruta que requiere
+
 
 module.exports = {create,list,index,replace,update,destroy,addActor};
 
-//Por ultimo exportamos todas dichas funciones
